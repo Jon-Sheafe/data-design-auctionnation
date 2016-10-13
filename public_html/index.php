a7a8f2521645b219b6680faaa8a25298ff392a95
Jon Sheafe <msckj@yahoo.com>
2016-10-13 11:00:56 -0600

    persona assignment

--- public_html/index.php
+++ public_html/index.php
@@ -6,17 +6,44 @@
 	</head>
 	<body>
 		<header><h1>Persona Assignment</h1></header>
-		<h2>Persona</h2>
-			<p><b>Name: </b>Carla Humpphys<br>
-			<b>Age:</b> 45</br></p>
-			<p><b>Profession: </b> Carla owns her own thift store in  Tuson Arizona which is located in the SouthWest part of the United. Carla sells gentally used items to the general public. She also sells used office equipment, which is a part of her buiness has seen a lot of growth since the economy decline in 2008. <br></p>
+
+			<h2>Persona</h2>
+				<p><b>Name: </b>Carla Humpphys<br>
+				<b>Age:</b> 45</br></p>
+				<p><b>Profession: </b> Carla owns her own thift store in  Tuson Arizona which is located in the SouthWest part of the United. Carla sells gentally used items to the general public. She also sells used office equipment, which is a part of her buiness has seen a lot of growth since the economy decline in 2008. <br></p>
 				<p><b>Techology: </b>Desktop HP bargin computer. The HP computer has 4gb of ram, and 250 GB hard drive. Carla also ownes a iphone 5S she brought second hand.</br></P>
-				<p><b>Attitudes and Behavior: </b>Carla works a lot of hours a day at her store. She does not believe in wasting money buying new items that will only depreciate.</p>
+				<p><b>Attitudes and Behavior: </b>Carla works a lot of hours a day at her store. She does not believe in wasting money buying new items that will only depreciate.</p>sc
 				<p><b>Frustrations and Needs</b> Carla is confortabily shopping online, but she has been burnt in the past buying items that were different from what was discribed. She prefers doing business locally where see can see the items before purchasing.</P>
 				<p><b>Goal: </b>Carla is only interested in purchasing local items for business. She does not have a lot of time as she runs the store 100% alone.</p><hr>
+
+
 			<h2>Use Case</h2>
-		<p>Carla bids on items from auctionnation.com using her desktop PC. She can purchase items on ebay, but perfers seeing items first and saving on shipping cost by picking up items from Auction Nation's wherehouses which are located mostly in Pheonix AZ. Carla does not only buy from Auction Nation, but keeps an eye on new actions for items that she could sell in her store. Carla wants to see pictures of items, and description of items in order to determine if its an item she will be interested in buying for her store.</p>
+				<p>Carla bids on items from auctionnation.com using her desktop PC. She can purchase items on ebay, but perfers seeing items first and saving on shipping cost by picking up items from Auction Nation's wherehouses which are located mostly in Pheonix AZ. Carla does not only buy from Auction Nation, but keeps an eye on new actions for items that she could sell in her store. Carla wants to see pictures of items, and description of items in order to determine if its an item she will be interested in buying for her store.</p>
+
+		<h2>Interaction Flow</h2>
+			<ol type="I">
+				<li>enter auctionnation.com into address bar</li>
+				<li>click on view Auction Link</li>
+				<li>scro</li>
+				<li>Images</li>
+				<li>Discription</li>
+				<li>bidder number, amount bidded</li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ol>
+
+		<h2>Conceptual Model</h2>
+
+			<h3>Identify Entities</h3>
+
 		<h2>Identify Entities</h2>
+
+
+
+
+
+
 	</body>
 </html>
 
